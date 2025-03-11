e98bc7df4fb044940753f98181e2e9d251b68fd1
AndresZ25 <andres.zun25@gmail.com>
2025-03-11 10:27:45 -0400

    last changes

--- Develop/index.js
+++ Develop/index.js
@@ -1,10 +1,9 @@
-// TODO: Include packages needed for this application
 const fs = require('fs');
 const inquirer = require('inquirer');
 const generateMarkdown = require('./utils/generateMarkdown');
 
 
-// TODO: Create an array of questions for user input
+
 const questions = [{
     type: 'input',
     name: 'title',
@@ -52,16 +51,16 @@ const questions = [{
     message: 'Enter your email address:',
   },];
 
-// TODO: Create a function to write README file
+
 function writeToFile(fileName, data) {fs.writeFile(fileName, data, (err) =>
     err ? console.error(err) : console.log('README.md generated successfully!')
   );}
 
-// TODO: Create a function to initialize app
+
 function init() { inquirer.prompt(questions).then((answers) => {
     const markdown = generateMarkdown(answers);
     writeToFile('README.md', markdown);
   });}
 
-// Function call to initialize app
+
 init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,26 +1,20 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
 function renderLicenseBadge(license) { if (license === 'None') return '';
   return `![License](https://img.shields.io/badge/license-${license}-blue.svg)`;}
 
 
   
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
+
 function renderLicenseLink(license) { if (license === 'None') return '';
   return `[Learn more about the ${license} license](https://choosealicense.com/licenses/${license.toLowerCase()}/)`;}
 
 
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
 function renderLicenseSection(license) {if (license === 'None') return '';
   return `## License
 This project is licensed under the ${license} license.`;}
 
 
 
-// TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
 
